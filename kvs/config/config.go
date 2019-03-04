f2154999906f29e42eed444bf20245585f7d8b7f
Vladimir Melnick <egotraumatic@gmail.com>
2019-03-04 09:52:58 +0200

    kvs updates

--- kvs/config/config.go
+++ kvs/config/config.go
@@ -1,5 +1,9 @@
 package config
 
+func ID() string {
+	return "node-1"
+}
+
 func ListenTo() string {
 	return ":9000"
 }
--- kvs/targets/server/main.go
+++ kvs/targets/server/main.go
@@ -4,13 +4,22 @@ import (
 	"net/http"
 
 	"github.com/egoholic/juggler/kvs/config"
+	"github.com/egoholic/juggler/kvs/node"
 )
 
 func main() {
-	app := NewApp()
+	app := newApp()
 	http.ListenAndServe(config.ListenTo(), app)
 }
 
-func NewApp() http.Handler {
-  
+func newApp() http.Handler {
+	return &App{node.New(config.ID())}
+}
+
+type App struct {
+	node *node.Node
+}
+
+func (*App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 }
